e7080c538217d0b9674b85b00df5fa4b0f534edd
Zach Hedeen <hedeenza@gmail.com>
2026-05-14 15:55:25 -0400

    added explanatory comments to the pinyin generation script

--- generate_pinyin.py
+++ generate_pinyin.py
@@ -1,11 +1,18 @@
 from xpinyin import Pinyin
 import sys
 
+# Open the hanzi.csv file, in argument 1 after the script name
+# Set to "read-only" and "utf-8" characters
 with open(sys.argv[1], 'r', encoding = 'utf-8') as file:
+    # Create a new file called "hanzi_pinyin.csv", which will contain the output
     with open('hanzi_pinyin.csv', 'a', encoding = 'utf-8') as output:
+        # for each line in the hanzi.csv file...
         for line in file:
-            pin = Pinyin().get_pinyin(line, splitter = ' ', tone_marks = 'marks')
-            output.write(f'{line.strip()},{pin}')
-
+            # Generate the pinyin, including the tone marks, and separate syllables by a space
+            pinyin = Pinyin().get_pinyin(line, splitter = ' ', tone_marks = 'marks')
+            # Write each line to the output file in the format "hanzi,pinyin"
+            output.write(f'{line.strip()},{pinyin}')
+    # Close the output file (hanzi_pinyin.csv)
     output.close()
+# Close the input file (hanzi.csv)
 file.close()
