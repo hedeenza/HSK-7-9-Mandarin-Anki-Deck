10f55f4a6452248794e01a5220bd936ff87d6593
Zach Hedeen <hedeenza@gmail.com>
2026-05-15 13:17:53 -0400

    completed and commented english and pinyin generation scripts

--- generate_english.py
+++ generate_english.py
@@ -1,5 +1,5 @@
-from xpinyin import Pinyin
 import sys
+from deep_translator import GoogleTranslator 
 
 # Choose a starting line for the translation. If no input, default to 1.
 starting_index = int(input(f'What line would you like to start with?\n') or 1)
@@ -7,9 +7,9 @@ starting_index = int(input(f'What line would you like to start with?\n') or 1)
 # Open the hanzi.csv file, in argument 1 after the script name
 # Set to "read-only" and "utf-8" characters
 with open(sys.argv[1], 'r', encoding = 'utf-8') as input:
-    # Create a new file called "hanzi_pinyin.tsv", which will contain the output
+    # Create a new file called "hanzi_english.tsv", which will contain the output
     # Set to "append" mode, so the translation can be safely resumed if interrupted
-    with open('hanzi_pinyin.tsv', 'a', encoding = 'utf-8') as output:
+    with open('hanzi_english.tsv', 'a', encoding = 'utf-8') as output:
         # for each line in the hanzi.csv file...
         for index, line in enumerate(input):
             # If the current index is less than the desired starting index...
@@ -17,17 +17,17 @@ with open(sys.argv[1], 'r', encoding = 'utf-8') as input:
             if index < starting_index - 1:
                 # Skip it
                 continue
-            # Otherwise... 
+            # Otherwise...
             else: 
                 # Strip the line down to its primary text
                 original_text = line.strip()
-                # Generate the pinyin for the line, including the tone marks, and separate syllables by a space
-                pinyin = Pinyin().get_pinyin(line, splitter = ' ', tone_marks = 'marks')
-                # Write each line to the output file in the format "hanzi,pinyin"
-                output.write(f'{original_text}\t{pinyin}')
-                # Print the line number, original text, and pinyin to mark progress
-                print(f'{index + 1} - {original_text}\t{pinyin}')
-    # Close the output file (hanzi_pinyin.tsv)
+                # Perform the translation from Mandarin to English 
+                english_text = GoogleTranslator(input = 'zh-CN', output = 'en').translate(original_text)
+                # Write to File
+                output.write(f'{original_text}\t{english_text}\n')
+                # Print the line number, original text, and English to mark progress
+                print(f'{index + 1} - {original_text}\t{english_text}')
+    # Close the output file (hanzi_english.tsv)
     output.close()
-# Close the input file (hanzi.csv)
+# Close the input file (hanzi.tsv)
 input.close()
